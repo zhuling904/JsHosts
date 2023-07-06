0b7f74cc074e4999ee9437e26fc6b892893fd1ff
zhuling822 <ling.zhu@js.design>
2023-07-06 11:55:53 +0800

    fix: 去除无用代码

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const { init } = require("./init");
 const { inquirerCmd } = require('./inquirerCmd');
 
 async function main() {
-    const { hostsList, configList } = await init();
     inquirerCmd()
 }
 
