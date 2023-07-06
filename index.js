5b245f7ac4f3b0f7ff6582e11a61cf3f3a75df90
zhuling822 <ling.zhu@js.design>
2023-07-06 19:46:11 +0800

    feat: 权限判断&授予当前用户hosts的写入权限

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-const { inquirerCmd } = require('./inquirerCmd');
-
+const { checkWrPermission } = require('./utils/checkWrPermission');
 async function main() {
-    inquirerCmd()
+    checkWrPermission()
 }
-
 main()
---
+++ utils/checkWrPermission.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const filePath = '/etc/hosts';
+const { exec } = require('child_process');
+const { inquirerCmd } = require('jshosts/inquirerCmd');
+
+// 授权以将hosts改为当前用户可读写
+const command = 'chmod u+rw /etc/hosts';
+
+async function checkWrPermission() {
+    return new Promise((resolve, reject) => {
+        fs.access(filePath, fs.constants.W_OK, (err) => {
+            if (err) {
+                reject(err)
+                // 以管理员权限运行命令,将hosts改为当前用户可读写
+                exec(`sudo ${command}`, (error) => {
+                    if (error) {
+                        console.error(`命令执行错误: ${error}`);
+                        return;
+                    }
+                    console.log('授权成功');
+                });
+            }
+            inquirerCmd();
+        });
+    })
+}
+
+exports.checkWrPermission = checkWrPermission;
